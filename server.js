f39eb01765987cdb4a93e51e0e4d768c5f12d701
eymerin <garrett.bryce.young@gmail.com>
2023-11-29 13:38:16 -0700

    added demo video

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
+ 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening on PORT 3001"));
